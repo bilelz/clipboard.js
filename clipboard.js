aca06bd35fef767bb341734fdfe407e2724c89a5
Bilelz <bilelz@gmail.com>
2023-06-20 18:24:01 +0200

    init

---
+++ clipboard.js
@@ -0,0 +1,52 @@
+const copyTextBtn = document.getElementById("copyText");
+const copyTextImageBtn = document.getElementById("copyImage");
+
+copyTextBtn.addEventListener("click", () => {
+  copyText();
+});
+copyTextImageBtn.addEventListener("click", () => {
+  copyImage();
+});
+
+if (!("write" in navigator.clipboard)) {
+  document.getElementById("clipboardNotSupported").style.display = "block";
+}
+
+async function copyImage() {
+  if ("write" in navigator.clipboard) {
+    try {
+      const file = await (await fetch("coffee.png")).blob();
+      const data = [new ClipboardItem({ [file.type]: file })];
+      await navigator.clipboard.write(data);
+      copyTextImageBtn.innerHTML = "<span>👌</span> Copied!";
+    } catch (err) {
+      copyTextImageBtn.innerHTML = "<span>😭</span> Error copying image";
+
+      console.error("Failed to copy image: ", err);
+    }
+  } else {
+    copyTextImageBtn.innerHTML = "<span>😭</span> ClipboardItem not supported";
+  }
+
+  setTimeout(() => {
+    copyTextImageBtn.innerHTML =
+      "<span>📋</span> Copy the picture to your clipboard";
+  }, 3000);
+}
+
+async function copyText() {
+  const text = document.getElementById("text");
+
+  try {
+    await navigator.clipboard.writeText(text.value);
+    copyTextBtn.innerHTML = "<span>👌</span> Copied!";
+    document.getElementById("textPaste").value = "";
+  } catch (err) {
+    copyTextBtn.innerHTML = "<span>😭</span> Error copying text";
+    console.error("Failed to copy: ", err);
+  }
+
+  setTimeout(() => {
+    copyTextBtn.innerHTML = "<span>📋</span> Copy the text to your clipboard";
+  }, 3000);
+}
